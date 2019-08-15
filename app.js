f7aa5906dcc72a74813fd45a868f425369ef3ff3
Zhurbey Aleksandr <AZhurbey@bell-main.bellintegrator.ru>
2019-08-15 20:12:03 +0400

    Настройка

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.listen(3001, () => {
+app.listen(3210, () => {
   console.log('Сервер запущен на порту 3001');
 });
 
 app.get('/', (req, res) => {
   console.log('Сделали запррос');
+  res.send('Бэк для чата почти готов');
+});
+
+app.get('/setLogin', (req, res) => {
+  console.log('Сделали запррос на установку логина');
   res.send('Бэк для чата почти готов');
 });
